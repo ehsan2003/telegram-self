d02a6e9332de13b668a4076dd15b9a929cd8fbc5
ehsan <ehsan2003.2003.382@gmail.com>
2021-10-11 23:24:56 +0330

    adds validation to SpamStarter.ts

--- src/EventHandlers/Commands/SpamStarter.ts
+++ src/EventHandlers/Commands/SpamStarter.ts
@@ -2,17 +2,40 @@ import {CommandHandlerBase} from "./CommandHandler.base";
 import {CommandHandlerFactory} from "./CommandHandlerFactory.base";
 import {NewMessageEvent} from "telegram/events";
 import yargs from "yargs";
-import {SpamProcess} from "../../Processes/SpamProcess";
+import {Args as SpamProcessArgs, SpamProcess} from "../../Processes/SpamProcess";
+import {SelfError} from "../../SelfError";
 
 export type Args = {
     interval: number;
     textCategory: string;
-    chat: number;
+    chat: string;
+    name?: string;
 };
 
 export class SpamStartHandler extends CommandHandlerBase<Args> {
     protected async execute(): Promise<void> {
-        this.ctx.processManager.run(new SpamProcess(this.ctx, this.args))
+        const sanitizedArguments = await this.validateAndSanitizeArgumentsOrThrow();
+        this.ctx.processManager.run(new SpamProcess(this.ctx, sanitizedArguments), this.args.name);
+    }
+
+    private async validateAndSanitizeArgumentsOrThrow(): Promise<SpamProcessArgs> {
+        let chatResolver: number | string = this.args.chat;
+        if (!isNaN(+this.args.chat)) {
+            chatResolver = +this.args.chat;
+        }
+
+        const chat = await this.ctx.client.getEntity(chatResolver).catch(() => null);
+        if (!chat) {
+            throw new SelfError('chat not found');
+        }
+
+        const textCategory = await this.ctx.prisma.preparedTextCategory.findFirst({where: {name: this.args.textCategory}});
+        if (!textCategory) {
+            throw new SelfError('text category not found');
+        }
+
+        return {...this.args, chat: chat.id};
+
     }
 }
 
@@ -25,7 +48,8 @@ export class SpamStartHandlerFactory extends CommandHandlerFactory {
         return yargs
             .option('interval', {alias: 'i', default: 500, number: true,})
             .option('textCategory', {alias: 't', string: true, demandOption: true})
-            .option('chat', {alias: 'c', number: true, demandOption: true})
+            .option('chat', {alias: 'c', string: true, demandOption: true,})
+            .option('name', {alias: 'n', string: true, demandOption: false})
     }
 
     protected getName(): string {
