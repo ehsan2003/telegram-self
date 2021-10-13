9878ef3102be6711ec5d8e15c2c32aaec0232af8
ehsan <ehsan2003.2003.382@gmail.com>
2021-10-13 20:03:40 +0330

    adds CommandRepresentation abstract class for extending CommandFactory

---
+++ src/CommandBehaviours/CommandRepresentation.ts
@@ -0,0 +1,24 @@
+import {ICommandFactory} from "./CommandFactory";
+import {ICommandHandler} from "./ICommandHandler";
+import {NewMessageEvent} from "telegram/events";
+import {detailed, DetailedArguments, Options} from "yargs-parser";
+import {SelfError} from "../SelfError";
+
+export abstract class CommandRepresentation<ParsedArgs extends DetailedArguments, ValidatedArguments> implements ICommandFactory {
+    async createHandler(event: NewMessageEvent, rawArguments: string[]): Promise<ICommandHandler> {
+        const argumentsOptions = this.getArgumentsOptions();
+        const args = detailed(rawArguments, argumentsOptions);
+        if (args.error) {
+            throw new SelfError(args.error.message);
+        }
+        const validatedArguments = await this.validateArguments(args as ParsedArgs);
+        return this.factory(event, validatedArguments);
+    }
+
+    abstract factory(event: NewMessageEvent, validatedArguments: ValidatedArguments): Promise<ICommandHandler> | ICommandHandler;
+
+    abstract getArgumentsOptions(): Options;
+
+    abstract validateArguments(parsedArgs: ParsedArgs): Promise<ValidatedArguments> | ValidatedArguments;
+
+}
