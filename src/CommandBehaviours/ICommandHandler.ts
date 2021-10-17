b1fa97f5793f4918f44ba4b25d562bed93d5ec67
ehsan <ehsan2003.2003.382@gmail.com>
2021-10-17 12:34:31 +0330

    adds a method to ICommandHandler for getting a short help about command

--- src/CommandBehaviours/ICommandHandler.ts
+++ src/CommandBehaviours/ICommandHandler.ts
@@ -2,4 +2,6 @@ import {MessageLike} from "./MessageLike";
 
 export interface ICommandHandler {
     handle(messageLike: MessageLike, args: string[]): Promise<void>;
+
+    getShortHelp(): string;
 }
