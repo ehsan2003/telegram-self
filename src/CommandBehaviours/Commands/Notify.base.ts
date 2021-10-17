6f7a4038ee68bb3373b062de29a9faecc554f10f
ehsan <ehsan2003.2003.382@gmail.com>
2021-10-17 13:02:31 +0330

    migrates NotifyBase

--- src/CommandBehaviours/Commands/Notify.base.ts
+++ src/CommandBehaviours/Commands/Notify.base.ts
@@ -1,25 +1,51 @@
-import {Context} from "../../Context";
-import {NewMessageEvent} from "telegram/events";
 import {Api, utils} from "telegram";
 import {chunk} from "lodash";
 import {sleep} from "telegram/Helpers";
+import {BaseCommandHandler} from "../BaseCommandHandler";
+import {MessageLike} from "../MessageLike";
+import yargsParser from "yargs-parser";
+import {validateJoi} from "../../utils";
+import * as Joi from 'joi';
 
 export type NotifyBaseArgs = {
     countPerMessage: number;
     delayBetweenMessages: number;
 }
 
-export class NotifyBase {
-    constructor(protected ctx: Context, protected event: NewMessageEvent, protected args: NotifyBaseArgs) {}
+export abstract class NotifyBase<Parsed extends Partial<NotifyBaseArgs>, Validated extends NotifyBaseArgs> extends BaseCommandHandler<Parsed, Validated> {
+    protected async execute(message: MessageLike, validatedArgs: Validated): Promise<void> {
+        const users = await this.getUsersForMention(message, validatedArgs)
+        await this.notifyUsers(users, message, validatedArgs);
+    }
 
-    protected async notifyUsers(participants: Api.User[]) {
-        const chunks = chunk(participants, this.args.countPerMessage)
+    protected getArgsParserOptions(): yargsParser.Options {
+        return {
+            number: ['countPerMessage', 'delayBetweenMessages'],
+            alias: {
+                countPerMessage: 'c',
+                delayBetweenMessages: 'd',
+            }
+        }
+    }
+
+    abstract getUsersForMention(message: MessageLike, args: NotifyBaseArgs): Promise<Api.User[]>;
+
+    protected validateParsedArgs(parsedArgs: Parsed): Validated {
+        return validateJoi(Joi.object({
+            countPerMessage: Joi.number().default(5),
+            delayBetweenMessages: Joi.number().default(1000)
+        }), parsedArgs)
+    }
+
+
+    protected async notifyUsers(participants: Api.User[], message: MessageLike, args: Validated) {
+        const chunks = chunk(participants, args.countPerMessage)
         for (const chunk of chunks) {
-            await this.ctx.client.sendMessage(this.event.chatId!, {
+            await this.ctx.client.sendMessage(message.chatId, {
                 message: this.getMessageForChunk(chunk),
                 parseMode: 'html'
             })
-            await sleep(this.args.delayBetweenMessages);
+            await sleep(args.delayBetweenMessages);
         }
     }
 
