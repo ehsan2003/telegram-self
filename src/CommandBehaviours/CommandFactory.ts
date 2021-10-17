7022cd85df3d5d5151eb46be57e8ae0b29cbfce0
ehsan <ehsan2003.2003.382@gmail.com>
2021-10-17 14:25:55 +0330

    removes legacy CommandFactory interface

--- src/CommandBehaviours/CommandFactory.ts
+++
@@ -1,6 +0,0 @@
-import {ICommandHandler} from "./ICommandHandler";
-import {NewMessageEvent} from "telegram/events";
-
-export interface ICommandFactory {
-    createHandler(event: NewMessageEvent, rawArguments: string[]): Promise<ICommandHandler> | ICommandHandler;
-}
